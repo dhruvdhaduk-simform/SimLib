b7c1c22eb65b90e0d42937fd14315508269213fb
Inshiya Ravat <inshiya.ravat@simformsolutions.com>
2025-05-19 18:06:12 +0530

    final configurations

--- lib/components/Button/Button.tsx
+++ lib/components/Button/Button.tsx
@@ -1,10 +1,13 @@
 import {css} from '@emotion/react'
+import Test from '../../assets/test.svg'
 
 const style = css`
-    color: 'red';
+    color: red;
 `
 export const Button = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
-    <button css={style} {...props}/>
+    <button css={style} {...props}>
+      <img src={Test} alt='abc'/>
+    </button>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-// import {resolve} from 'path'
+import {resolve} from 'path'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 
@@ -9,18 +9,18 @@ export default defineConfig({
     react(),
     dts({tsconfigPath: './tsconfig.app.json', include: ['lib']})
   ],
-  // build:{
-  //   lib:{
-  //     entry: resolve(__dirname, 'lib/main.ts'),
-  //     formats:['es']
-  //   },
-  //   copyPublicDir: false,
-  //   rollupOptions: {
-  //     external: ['react', 'react/jsx-runtime','@emotion/react','@emotion/styled'],
-  //   },
-  // },
+  build:{
+    lib:{
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats:['es']
+    },
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime','@emotion/react','@emotion/styled'],
+    },
+  },
   esbuild:{
     jsx:'automatic',
     jsxImportSource: '@emotion/react'
-  }
+  },
 })
